ab30cc3880205ff429125399fcbcc824617ea54c
Saumendra_Raul <134996683+SaumendraRaul@users.noreply.github.com>
2026-09-17 02:22:13 +0530

    chore: bump PWA cache for scroll fix

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
-const CACHE="weird-chess-v3-5-git";
+const CACHE="weird-chess-v3-5-git-scrollfix-1";
 const CORE=["./","./index.html","./app-base-a.css","./app-base-b.css","./app-extra.css","./game-seed.js","./game-core-1.js","./game-core-2.js","./game-core-3.js","./game-core-4.js","./game-bot.js","./game-v34-a.js","./game-v34-b.js","./game-v35.js","./manifest.json","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener("fetch",e=>{
   e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(resp=>{
